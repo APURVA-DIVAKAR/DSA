26745289eadac0da6c08f2768581dba0069bc885
Apurva Divakar <apurvadiwaker@gmail.com>
2026-03-22 00:48:52 +0530

    Time: 26 ms (38.82%), Space: 74.2 MB (25.55%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -4,19 +4,31 @@
  */
 var findDuplicates = function(nums) {
 
-    nums.sort((a,b)=> a-b);
-    let i = 0;
+    let seen = new Set();
     let res = [];
-    
-    while(i < nums.length-1){
-        if(nums[i] === nums[i+1]) {
-            res.push(nums[i]);
-            i += 2;
+
+    for(let num of nums) {
+        if(seen.has(num)){
+            res.push(num);
         } else {
-            i++;
+            seen.add(num)
         }
     }
 
     return res;
+    // nums.sort((a,b)=> a-b);
+    // let i = 0;
+    // let res = [];
+    
+    // while(i < nums.length-1){
+    //     if(nums[i] === nums[i+1]) {
+    //         res.push(nums[i]);
+    //         i += 2;
+    //     } else {
+    //         i++;
+    //     }
+    // }
+
+    // return res;
     
 };
