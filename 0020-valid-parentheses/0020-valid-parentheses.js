506c19dc1f4823f2fb9027c00f2ec2fd8a690d65
Apurva Divakar <apurvadiwaker@gmail.com>
2026-03-27 14:50:04 +0530

    Time: 1 ms (91.8%), Space: 54.8 MB (86.27%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+    let stack = [];
+
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for(let i=0;i<s.length;i++) {
+        if(s[i] === "(" || s[i] === "{" || s[i] === "["){
+            stack.push(s[i]);
+        } else {
+            if (stack.length === 0 || stack.pop() !== map[s[i]]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length===0;
+    
+};
